64b57e57bff5e738cbe35c7d6758beb75034b85c
estercalveras <estercalveras@gmail.com>
2024-08-15 19:44:34 +0200

    Changes Ester

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import LoginModal from "./LoginModal";
 
-const Header = () => {
+function Header({ toggleDarkMode }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div>
-      Header
+    <header className="bg-white dark:bg-gray-900 p-4 shadow-md flex justify-between items-center">
+      <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
+        <a href="/">platevista</a>
+      </h1>
+      <div className="flex items-center space-x-4">
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="text-orange-500 dark:text-orange-400"
+        >
+          Login
+        </button>
+        <button
+          onClick={toggleDarkMode}
+          className="text-gray-900 dark:text-white"
+        >
+          Toggle Dark Mode
+        </button>
     </div>
-  )
+      {isModalOpen && <LoginModal onClose={() => setIsModalOpen(false)} />}
+    </header>
+  );
 }
 
-export default Header
+export default Header;
