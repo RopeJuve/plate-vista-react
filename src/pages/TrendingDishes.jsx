085f3b7f6c96f8d37e7f5b8145b62928068e2840
RopeJuve <rope.s@hotmail.com>
2024-09-05 20:48:21 +0200

    Fixed the imports

--- src/pages/TrendingDishes.jsx
+++ src/pages/TrendingDishes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Pie from '../pages/Charts/Pie';
-import Header from '../components/AdminComponents/Header';
+import Header from '../Components/AdminComponents/Header';
 
 const TrendingDishes = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-d-main-bg rounded-3xl shadow-lg transition-colors duration-300 ease-in-out">
