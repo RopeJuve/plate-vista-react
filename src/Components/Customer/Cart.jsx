9d6b10cf938ea5b195cee4ddabb9fdb56e1550b5
RopeJuve <rope.s@hotmail.com>
2024-09-10 00:19:09 +0200

    New Cart Design and Ordering Connection

--- src/Components/Customer/Cart.jsx
+++ src/Components/Customer/Cart.jsx
@@ -8,15 +8,16 @@ const Cart = () => {
   const [showCart, setShowCart] = useState(false);
   return (
     <>
-      {cart.length > 0 && (
-        <div
-          className="fixed bottom-[1rem] right-[0.5rem] z-50 w-10 h-10 rounded-full cursor-pointer bg-orange-400 p-8"
+       <div
+          className="fixed bottom-[1rem] right-[0.5rem] z-50 w-10 h-10 rounded-full cursor-pointer bg-orange-400 p-7 shadow-red-400 shadow-lg"
           onClick={() => setShowCart(!showCart)}
         >
-          <MdOutlineShoppingCart className="text-white w-8 h-8 translate-x-[-50%] translate-y-[-50%]" />
-          {showCart && <CartModal items={cart} />}
+          <MdOutlineShoppingCart className="text-white w-8 h-8 translate-x-[-50%] translate-y-[-50%] relative" />
+          {cart.length > 0 && (<div
+            className="absolute z-[50] top-0 right-0 text-[0.725rem] bg-red-500 text-white w-4 h-4 rounded-full flex items-center justify-center"
+          >{cart.length}</div>)}
+          {showCart && <CartModal items={cart} closeModal={setShowCart} />}
         </div>
-      )}
     </>
   );
 };
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -2,32 +2,18 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
-export function useCart() {
-  return useContext(CartContext);
-}
+export const useCart = () => useContext(CartContext);
+
 
 export function CartProvider({ children }) {
   const [cart, setCart] = useState(
     localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
   );
 
-  const [orders, setOrders] = useState(
-    localStorage.getItem("orders")
-      ? JSON.parse(localStorage.getItem("orders"))
-      : []
-  );
-
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  useEffect(() => {
-    localStorage.setItem("orders", JSON.stringify(orders));
-  }, [orders]);
-
-  const addOrder = (cart) => {
-    setOrders([...orders, cart]);
-  };
   const addToCart = (item) => {
     setCart((prevCart) => {
       const itemExistIndex = prevCart.findIndex((i) => i._id === item._id);
@@ -49,12 +35,9 @@ export function CartProvider({ children }) {
   const clearCart = () => {
     setCart([]);
   };
-  const clearOrders = () => {
-    setOrders([]);
-  };
   return (
     <CartContext.Provider
-      value={{ cart, addToCart, removeFromCart, clearCart, addOrder, orders, clearOrders }}
+      value={{ cart, addToCart, removeFromCart, clearCart }}
     >
       {children}
     </CartContext.Provider>
