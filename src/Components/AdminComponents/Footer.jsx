c19a12e9899fa314b4e6c2987e29291759aa2403
Petar Petrov <peterpetrov1985@gmail.com>
2024-10-07 11:30:53 +0200

    fixed footer positioning

--- src/Components/AdminComponents/Footer.jsx
+++ src/Components/AdminComponents/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <div>
-    <p className="dark:text-gray-200 text-gray-700 text-center m-2">
+    <p className="absolute bottom-0 w-full dark:text-gray-200 text-gray-700 text-center">
       © 2024 All rights reserved by platevista.com
     </p>
   </div>
--- src/Components/Customer/MenuItemsList.jsx
+++ src/Components/Customer/MenuItemsList.jsx
@@ -2,7 +2,7 @@ import MenuItemCard from "./MenuItemCard";
 
 const MenuItemsList = ({ items, isLoading }) => {
   return (
-    <div className="px-6 mt-6 w-full h-full overflow-y-auto">
+    <div className="px-6 mt-6 w-full h-full pb-16 overflow-y-auto">
       <div className="grid grid-flow-row auto-rows-max grid-cols-card gap-3">
         {!isLoading && items.map(item => (
           <MenuItemCard key={item._id} item={item} />
